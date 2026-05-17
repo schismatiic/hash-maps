2eae0518e9808b93cb10d70b2e4c83cb4ec69913
schismatiic <leonardojofre2003@gmail.com>
2026-05-16 23:24:57 -0400

    set(key, value) created

--- src/HashMap.js
+++ src/HashMap.js
@@ -1,7 +1,11 @@
+import { LinkedList } from "./LinkedList.js";
+
 class HashMap {
+  #buckets = [];
   constructor(load_factor = 0.75, capacity = 16) {
     this.load_factor = load_factor;
     this.capacity = capacity;
+    this.#buckets = new Array(this.capacity).fill(null);
   }
   hash(key) {
     let hashCode = 0;
@@ -10,6 +14,28 @@ class HashMap {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     }
     return hashCode;
+  }
+  set(key, value) {
+    const hashed_key = this.hash(key);
+    const index = hashed_key % this.capacity;
+    if (index < 0 || index >= this.#buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    if (this.#buckets[index] === null) {
+      const list = new LinkedList();
+      list.append([key, value]);
+      this.#buckets[index] = list;
+      this.#buckets[index].toString();
+    } else {
+      const exists = this.#buckets[index].contains(key);
+      if (exists) {
+        this.#buckets[index].replace(key, value);
+        this.#buckets[index].toString();
+      } else {
+        this.#buckets[index].toString();
+        this.#buckets[index].append([key, value]);
+      }
+    }
   }
 }
 export { HashMap };
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { HashMap } from "./HashMap.js";
+
+const hash_map = new HashMap();
+hash_map.set("apple", "red");
+hash_map.set("banana", "yellow");
+hash_map.set("carrot", "orange");
+
+hash_map.set("apple", "green");
+hash_map.set("banana", "black");
+
+hash_map.set("ab", "first");
+hash_map.set("ba", "second");
+hash_map.set("abc", "third");
+hash_map.set("cab", "fourth");
+
+hash_map.set("ba", "UPDATED");
